b8f23c41b3ee1fe6b43f967ccd8c924847831a62
Wolf Syntax <wolfsyntax@yahoo.com>
2019-06-28 21:40:51 +0800

    Fix registration button

--- static/static/javascript/custom.js
+++ static/static/javascript/custom.js
@@ -1,4 +1,4 @@
-function sys_timer(){
+/*function sys_timer(){
 //	var x = document.getElementById("timeDiff").innerText;
 //	document.getElementById("timeDiff").innerHTML = x;
   	//alert("test" + x);
@@ -27,6 +27,7 @@ function checkTime(i) {
        // alert("test");
     }
 */
+/*
     $('#sidebarCollapse').on('click', function () {
 
         $('#sidebar').toggleClass('active');
@@ -41,31 +42,32 @@ function checkTime(i) {
         //alert("Toggle");
     });
 
-
+*/
 $(document).ready(function(){
 
 	function sys_timer(){
 //	var x = document.getElementById("timeDiff").innerText;
 //	document.getElementById("timeDiff").innerHTML = x;
   	//alert("test" + x);
-  var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
-  h = checkTime(h);
-  m = checkTime(m);
-  s = checkTime(s);
-  document.getElementById('timeDiff').innerHTML =
-  h + ":" + m + ":" + s;
-//  var t = setTimeout(startTime, 500);
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    h = checkTime(h);
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('timeDiff').innerHTML =
+    h + ":" + m + ":" + s;
+  //  var t = setTimeout(startTime, 500);
 
-   	var t = setTimeout(sys_timer, 500);
-}
+     	var t = setTimeout(sys_timer, 500);
 
-function checkTime(i) {
-  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-  return i;
-}
+  }
+
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
 
 /*	function toggler() {
         $('#sidebar').toggleClass('active');
@@ -73,44 +75,30 @@ function checkTime(i) {
        // alert("test");
     }
 */
-    $('#sidebarCollapse').on('click', function () {
 
-        $('#sidebar').toggleClass('active');
-        $('#body-container').toggleClass('active');
-        var x = $("#toggler-icon").attr("class");
-        if(x == 'fa fa-arrow-left fa-lg'){
-        	$("#toggler-icon").removeClass("fa fa-arrow-left fa-lg").addClass("fa fa-arrow-right fa-lg");
-		}else{
-        	$("#toggler-icon").removeClass("fa fa-arrow-right fa-lg").addClass("fa fa-arrow-left fa-lg");
-		}
+  $("#is_agree").change(function(){alert("Changin");
+    if(this.checked){
+      $("#reg_submit").removeAttr("disabled");
+    }else{
+      $("#reg_submit").attr("disabled","disabled");
+    }
+  });
+
+  $('#sidebarCollapse').on('click', function () {
+
+    $('#sidebar').toggleClass('active');
+    $('#body-container').toggleClass('active');
+    
+    var x = $("#toggler-icon").attr("class");
+    
+    if(x == 'fa fa-arrow-left fa-lg'){
+      $("#toggler-icon").removeClass("fa fa-arrow-left fa-lg").addClass("fa fa-arrow-right fa-lg");
+    }else{
+      $("#toggler-icon").removeClass("fa fa-arrow-right fa-lg").addClass("fa fa-arrow-left fa-lg");
+    }
 		//alert(x);
         //alert("Toggle");
-    });
-
-
-$(document).ready(function(){
-
-	if($("is_agree").val()) {
-		$("#reg_submit").removeAttr("disabled");
-	}else{
-		$("#reg_submit").attr("disabled", "disabled");
-	}
-
-	sys_timer();
-
-	$("#is_agree").change(function(){
-		if(this.checked){
-			$("#reg_submit").removeAttr("disabled");
-		}else{
-			$("#reg_submit").attr("disabled","disabled");
-		}
-	});
-
-	$('#dashboard_invoice').DataTable();
-
-
-});
-
+  });
 
 
 
